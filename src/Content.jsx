83ff4ce8e078b20ea8acf01a5c3608c24119807b
Thiago Carvalho <thiagoafonso07@hotmail.com>
2022-11-23 02:55:33 -0300

    a lot of changes, still wip 👺

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,20 +1,30 @@
 import styles from './content.module.css';
+import moment from 'moment';
 
-export function Content() { 
+
+
+export function Content() {
+    var timeStartedDev = moment("2022-07-06T18:00:00Z");
+    var timeNow = moment();
+    const hoursSinceStartedDev = (timeNow.diff(timeStartedDev, 'hours'))
+
+    // console.log(moment("2022-07-06T18:00:00Z").startOf('hour').fromNow())
+    // console.log(moment.duration("2022-07-06T18:00:00Z").asHours())
     return (
         <div className={styles.contentMainWrapper}>
-        <h1>
-                Lorem ipsum
-                <br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-
-        </h1>
-        <p className={styles.contentP}>
-            Lorem ipsum dolor sit okkkkkkkkkkkamet consectetur
-            <br />adipisicing elit. Cumque culpa unde
-            <br />laudantium quasi blanditiis at illum asperiores error 
-            <br />obcaecati totam iusto tempora nostrum, incidunt eius quae, quisquam magni aut fugit.
-        </p>
+            <div className={styles.contentIntro}>
+                <span className={styles.IntroHello}>Hello, my name is </span>
+                <span className={styles.IntroName}>Thiago Carvalho</span>
+            </div>
+            <h1 className={styles.frontTitle}>
+                Frontend Developer
+            </h1>
+            <div className={styles.hoursSinceDiv} >
+            <span className={styles.hoursSince}>{hoursSinceStartedDev}</span>
+            <span className={styles.hoursSinceText}>
+                Hours<br />spent<br />coding
+            </span>
+            </div>
         </div>
     )
 }
