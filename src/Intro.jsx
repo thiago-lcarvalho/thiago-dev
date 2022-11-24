d24e7351f28ee8b6d8238486c26a6f625df14d7a
Thiago Carvalho <thiagoafonso07@hotmail.com>
2022-11-23 21:11:42 -0300

    🐴🐍🐤

---
+++ src/Intro.jsx
@@ -0,0 +1,28 @@
+import styles from './intro.module.css';
+import moment from 'moment';
+import { Hourglass } from 'phosphor-react';
+
+
+
+export function Intro() {
+    var timeStartedDev = moment("2022-07-06T18:00:00Z");
+    var timeNow = moment();
+    const hoursSinceStartedDev = (timeNow.diff(timeStartedDev, 'hours'))
+
+
+    return (
+        <section>
+        <div className={styles.introMainWrapper}>
+            <div>
+            <div className={styles.contentIntro}>
+                <span className={styles.IntroHello}>Hello, my name is </span>
+                <span className={styles.IntroName}>Thiago Carvalho</span>
+            </div>
+            <h1 className={styles.frontTitle}>
+                Frontend Developer
+            </h1>
+            </div>
+        </div>
+        </section>
+    )
+}
