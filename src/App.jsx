686dc95469dc861aac2b3ad4d5cb0bb1e03548cd
Thiago Carvalho <thiagoafonso07@hotmail.com>
2022-12-09 18:49:24 -0300

    added info

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ export function App() {
   return (
       <>
       <Header />
-      <First />
-      <Second />
-      <Third />
+      <First /> {/* add photo */}
+      <Second /> {/* change icon organization */}
+      <Third /> {/* add 3 cards with text and a paragraph at the bottom with contact purpose/info +button to email */}
       </>
   )
 }
