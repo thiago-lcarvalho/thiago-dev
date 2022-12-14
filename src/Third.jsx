8b3aec50c673f3137f906bfb10bda45a44d89672
Thiago Carvalho <thiagoafonso07@hotmail.com>
2022-12-14 16:40:58 -0300

    updated cards, added git contribution tracker 👽

--- src/Third.jsx
+++ src/Third.jsx
@@ -1,8 +1,26 @@
 import styles from './third.module.css';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
+import GitHubCalendar from 'react-github-calendar';
 
 export function Third() {
+
+    const sinceStart = contributions => {
+        const currentYear = new Date().getFullYear();
+        const currentMonth = new Date().getMonth();
+        const shownMonths = 2;
+      
+        return contributions.filter(day => {
+          const date = new Date(day.date);
+          const monthOfDay = date.getMonth();
+      
+          return (
+            date.getFullYear() === currentYear &&
+            monthOfDay > currentMonth - shownMonths &&
+            monthOfDay <= currentMonth
+          );
+        });
+      };
+
+
 	return (
 		<section>
 			<a name="3"></a>
@@ -16,7 +34,26 @@ export function Third() {
 						<p>lorem blabla ok </p>
 					</div>
 				</div>
-				<div className={styles.box2Div}></div>
+                <a className={styles.box2link} href="https://github.com/thiago-lcarvalho" target='blank'>
+				<div className={styles.box2Div}>
+                <div className={styles.gitHubCalendar}>
+						<GitHubCalendar
+							username="thiago-lcarvalho"
+                            transformData={sinceStart}
+							hideColorLegend
+                            hideMonthLabels
+							blockSize={14}
+							labels={{
+								totalCount: '{{count}}',
+							}}
+						/>
+						<span className={styles.gitHubCalendarText}>
+                        github contributions <br />
+                        in the last 2 months
+						</span>
+					</div>
+                </div>
+                </a>
 				<div className={styles.box3Div}></div>
 			</div>
 		</section>
