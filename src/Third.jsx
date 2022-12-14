28609397b9224ca6d01fa1ff5b43a9d78b70afdb
Thiago Carvalho <thiagoafonso07@hotmail.com>
2022-12-14 10:45:49 -0300

    changes + fixes

--- src/Third.jsx
+++ src/Third.jsx
@@ -1,14 +1,22 @@
-import styles from './third.module.css'
+import styles from './third.module.css';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
 
 export function Third() {
-    return (
-        <>
-        <a name="3"></a>
-        <div className={styles.contentMainWrapper}>
-            <h1>
-                Now What?
-            </h1>
-        </div>
-        </>
-    )
+	return (
+		<section>
+			<a name="3"></a>
+			<div className={styles.contentMainWrapper}>
+				<div className={styles.box1Div}>
+                    <h1>lorem ipsum</h1>
+                    <p>lorem ipsum blablablabla ok ok</p>
+                    <p>lorem blabla ok </p>
+				</div>
+				<div className={styles.box2Div}>
+				</div>
+				<div className={styles.box3Div}>
+				</div>
+			</div>
+		</section>
+	);
 }
