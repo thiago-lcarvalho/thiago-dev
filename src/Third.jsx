0c5b3e02432f04dd436b0f075ab1644124577679
Thiago Carvalho <thiagoafonso07@hotmail.com>
2022-12-19 14:24:19 -0300

    added api loading check

--- src/Third.jsx
+++ src/Third.jsx
@@ -2,9 +2,30 @@ import { Planet, SpotifyLogo, CloudRain } from 'phosphor-react';
 import styles from './third.module.css';
 import GitHubCalendar from 'react-github-calendar';
 import resSpotify from './resources/spotify_api';
-import resNasa from './resources/nasa_api';
+import NASA_API_KEY from './nasaAPIkey';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export function Third() {
+	const [resNasa, setResNasa] = useState([]);
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		axios
+			.get(
+				`https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=` +
+					NASA_API_KEY
+			)
+			.then((res) => {
+				setResNasa(res);
+				setLoading(true);
+				console.log(loading);
+				console.log(resNasa);
+			});
+	}, []);
+	console.log(loading);
+	console.log(resNasa.data);
+
 	function randomAsteroid(arr) {
 		const randomIndex = Math.floor(Math.random() * arr.length);
 		const item = arr[randomIndex];
@@ -12,13 +33,16 @@ export function Third() {
 		return item;
 	}
 
-	const nasaAsteroidInfo = resNasa.data.near_earth_objects;
-	const nasaRandomAsteroid = randomAsteroid(nasaAsteroidInfo);
-	const nasaAsteroidName = nasaRandomAsteroid.name_limited;
-	const nasaAsteroidID = nasaRandomAsteroid.id;
-	const nasaAsteroidIDURL = `https://ssd.jpl.nasa.gov/tools/sbdb_lookup.html#/?sstr=${nasaAsteroidID}&view=VOPDCA`;
-	var nasaAsteroidHazardous =
-		nasaRandomAsteroid.is_potentially_hazardous_asteroid;
+	const nasaAsteroidInfo = loading ? resNasa.data.near_earth_objects : '';
+	const nasaRandomAsteroid = loading ? randomAsteroid(nasaAsteroidInfo) : '';
+	const nasaAsteroidName = loading ? nasaRandomAsteroid.name_limited : '';
+	const nasaAsteroidID = loading ? nasaRandomAsteroid.id : '';
+	const nasaAsteroidIDURL = loading
+		? `https://ssd.jpl.nasa.gov/tools/sbdb_lookup.html#/?sstr=${nasaAsteroidID}&view=VOPDCA`
+		: '';
+	var nasaAsteroidHazardous = loading
+		? nasaRandomAsteroid.is_potentially_hazardous_asteroid
+		: '';
 
 	if (nasaAsteroidHazardous == true) {
 		nasaAsteroidHazardous = 'Yes';
@@ -27,9 +51,6 @@ export function Third() {
 	} else {
 		nasaAsteroidHazardous = 'Unknown';
 	}
-
-	console.log(nasaRandomAsteroid);
-	console.log(nasaAsteroidHazardous);
 
 	const spotifyTrack = resSpotify.data.items[0].track.name;
 	const spotifyTrackURL =
@@ -56,76 +77,76 @@ export function Third() {
 
 	return (
 		<section>
-            
 			<a name="3"></a>
-            
+
 			<div className={styles.contentMainWrapper}>
-            <div className={styles.boxDivs}>
-				<div className={styles.box1Div}>
-					<div className={styles.nasaAPI}>
-						<a
-							href={nasaAsteroidIDURL}
-							target="blank"
-						>
-							<span>NASA API</span>
-							<Planet
-								size={40}
-								color="#093A8A"
-							/>
-							<span></span>
-						</a>
-					</div>
-					<SpotifyLogo
-						size={40}
-						color="#1CD760"
-					/>
-					<CloudRain
-						size={40}
-						color="#F0F2EE"
-						weight="fill"
-					/>
-				</div>
-				<a
-					className={styles.box2link}
-					href="https://github.com/thiago-lcarvalho"
-					target="blank"
-				>
-					<div className={styles.box2Div}>
-						<div className={styles.gitHubCalendar}>
-							<GitHubCalendar
-								color={'#4DF088'}
-								username="thiago-lcarvalho"
-								transformData={sinceStart}
-								hideColorLegend
-								hideMonthLabels
-								blockSize={14}
-								labels={{
-									totalCount: '{{count}}',
-								}}
-							/>
-							<span className={styles.gitHubCalendarText}>
-								github contributions <br />
-								in the last 2 months
-							</span>
+				<div className={styles.boxDivs}>
+					<div className={styles.box1Div}>
+						<div className={styles.nasaAPI}>
+							<a
+								href={nasaAsteroidIDURL}
+								target="blank"
+							>
+								<span>NASA API</span>
+								<Planet
+									size={40}
+									color="#093A8A"
+								/>
+								<span></span>
+							</a>
 						</div>
+						<SpotifyLogo
+							size={40}
+							color="#1CD760"
+						/>
+						<CloudRain
+							size={40}
+							color="#F0F2EE"
+							weight="fill"
+						/>
 					</div>
-				</a>
-				<div className={styles.box3Div}>
 					<a
-						href={spotifyTrackURL}
+						className={styles.box2link}
+						href="https://github.com/thiago-lcarvalho"
 						target="blank"
 					>
-						<img
-							src={spotifyTrackIMG}
-							alt=""
-						/>
-						<span>
-							{spotifyTrack} <br /> by <br /> {spotifyTrackArtist}
-						</span>
+						<div className={styles.box2Div}>
+							<div className={styles.gitHubCalendar}>
+								<GitHubCalendar
+									color={'#4DF088'}
+									username="thiago-lcarvalho"
+									transformData={sinceStart}
+									hideColorLegend
+									hideMonthLabels
+									blockSize={14}
+									labels={{
+										totalCount: '{{count}}',
+									}}
+								/>
+								<span className={styles.gitHubCalendarText}>
+									github contributions <br />
+									in the last 2 months
+								</span>
+							</div>
+						</div>
 					</a>
+					<div className={styles.box3Div}>
+						<a
+							href={spotifyTrackURL}
+							target="blank"
+						>
+							<img
+								src={spotifyTrackIMG}
+								alt=""
+							/>
+							<span>
+								{spotifyTrack} <br /> by <br />{' '}
+								{spotifyTrackArtist}
+							</span>
+						</a>
+					</div>
 				</div>
 			</div>
-            </div>
 		</section>
 	);
 }
--- src/resources/nasa_api.jsx
+++ src/resources/nasa_api.jsx
@@ -1,8 +1,11 @@
-import NASA_API_KEY from "../nasaAPIkey";
-import axios from "axios";
+import NASA_API_KEY from '../nasaAPIkey';
+import axios from 'axios';
 
-const resNasa = await axios.get(
-    `https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=`+NASA_API_KEY,   
-{})
+
+const resNasa = await axios
+	.get(
+		`https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=` + NASA_API_KEY,
+		{}
+    );
 
 export default resNasa;
