54ab86f7b4572fe0246119b376d2cacc12530461
Thiago Carvalho <thiagoafonso07@hotmail.com>
2023-01-03 23:01:59 -0300

    fix

--- src/First.jsx
+++ src/First.jsx
@@ -1,25 +1,26 @@
 import styles from './first.module.css';
 import Avatar from '@mui/material/Avatar';
 
-
-
-
 export function First() {
-
-    return (
-        <section>
-            <section className={styles.introMainWrapper}>
-                <div className={styles.subMainDiv}>
-                <Avatar className={styles.introPhoto} sx={{ width: 100, height: 100 }} alt="Thiago Afonso" src="././important-photo.jpeg"/>
-                    <div className={styles.contentIntro}>
-                        <span className={styles.IntroHello}>Hello, my name is </span>
-                        <span className={styles.IntroName}>Thiago Afonso</span>
-                    </div>
-                    <h1 className={styles.frontTitle}>
-                        Frontend Developer
-                    </h1>
-                </div>
-            </section>
-        </section>
-    )
+	return (
+		<section>
+			<section className={styles.introMainWrapper}>
+				<div className={styles.subMainDiv}>
+					<Avatar
+						className={styles.introPhoto}
+						sx={{ width: 150, height: 150 }}
+						alt="Thiago Afonso"
+						src="././self-photo.jpeg"
+					/>
+					<div className={styles.contentIntro}>
+						<span className={styles.IntroHello}>
+							Hello, my name is{' '}
+						</span>
+						<span className={styles.IntroName}>Thiago Afonso</span>
+					</div>
+					<h1 className={styles.frontTitle}>Frontend Developer</h1>
+				</div>
+			</section>
+		</section>
+	);
 }
